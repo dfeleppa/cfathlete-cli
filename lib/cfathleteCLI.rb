8043808d18e2645f29d77da5be7b7684d61f799c
dfeleppa <dfeleppa@gmail.com>
2020-03-08 18:26:36 -0400

    add cli file

--- lib/cfathleteCLI.rb
+++ lib/cfathleteCLI.rb
@@ -1,4 +1,5 @@
 require "cfathleteCLI/version"
+require "cfathleteCLI/cli"
 
 module CfathleteCLI
   class Error < StandardError; end
---
+++ lib/cfathleteCLI/cli.rb
@@ -0,0 +1,8 @@
+class CfathleteCli::CLI
+
+    def start
+        puts "Welcome to CF Athlete CLI"
+    end
+
+
+end   
