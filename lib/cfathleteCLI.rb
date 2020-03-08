f3eaae7d3bdd5cff3843e7d162368dea9945b624
dfeleppa <dfeleppa@gmail.com>
2020-03-08 18:20:13 -0400

    first commit

---
+++ lib/cfathleteCLI.rb
@@ -0,0 +1,6 @@
+require "cfathleteCLI/version"
+
+module CfathleteCLI
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cfathleteCLI/version.rb
@@ -0,0 +1,3 @@
+module CfathleteCLI
+  VERSION = "1.0.0"
+end
